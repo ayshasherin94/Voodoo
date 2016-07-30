cbcf9dcb706b6dc0e25f8d229b9c43706be012ce
KhadijaEbrahim <khadijaEbrahim@Khadijas-MacBook-Pro.local>
2016-07-30 16:07:41 -0400

    php page

---
+++ shoppingcart.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<link href="style.css" type="text/css" rel="stylesheet" />
+
+
+
+<head>
+    <title>YOUR CART</title>
+
+
+    <link rel="stylesheet" href="css/normalize.css">
+
+    <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
+
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+    <div class="page">
+        <span class="menu_toggle">
+    <i class="menu_open fa fa-bars fa-lg"></i>
+    <i class="menu_close fa fa-times fa-lg"></i>
+  </span>
+        <ul class="menu_items">
+          <li><a href="cure.html"><i class="icon fa fa-signal fa-2x"></i> CURE</a></li>
+          <li><a href="scripts.html"><i class="icon fa fa-signal fa-2x"></i> SCRIPTS</a></li>
+          <li><a href="dolls.html"><i class="icon fa fa-signal fa-2x"></i> DOLLS</a></li>
+          <li><a href="services.html"><i class="icon fa fa-heart fa-2x"></i> SERVICES</a></li>
+          <li><a href="story.html"><i class="icon fa fa-coffee fa-2x"></i> STORIES</a></li>
+
+        </ul>
+        <main class="content">
+            <div class="content_inner">
+
+              <div class="txt-heading">
+                  <h1><a href="index.html"><img src="globe.gif" alt="home" width="60px" height="60px"/></a>YOUR CART<a id="btnEmpty" href="shoppingcart.php?action=empty"><img src="cart.png" alt="cart" align="right"/ height="50px" width="50px">{0}</h1></a></div>
+
+
+
+
+
+
+
+
+            </div>
+        </main>
+    </div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+
+
+
+</body>
+
+</html>
