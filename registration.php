2c724159ec7a713312c0cb9f3948a029eecbc244
ayshasherin94 <ayshasherin94@yahoo.com>
2016-07-30 21:41:19 -0400

    Registration

--- registration.php
+++ registration.php
@@ -1,21 +1,48 @@
 
 <!DOCTYPE html>
 <html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="styles/style.css">
+
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel='stylesheet prefetch' href='http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css'>
+    <link rel="stylesheet" href="css/style.css">
 <head>
-<meta charset="utf-8">
-<title>Registration</title>
-<link rel="stylesheet" href="css/style.css" />
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+	<title>Registration</title>
 </head>
 <body>
+  <div class="page">
+        <span class="menu_toggle">
+    <i class="menu_open fa fa-bars fa-lg"></i>
+    <i class="menu_close fa fa-times fa-lg"></i>
+  </span>
+        <ul class="menu_items">
+          <li><a href="cure.php"><i class="icon fa fa-signal fa-2x"></i> CURE</a></li>
+          <li><a href="scripts.php"><i class="icon fa fa-signal fa-2x"></i> SCRIPTS</a></li>
+          <li><a href="dolls.php"><i class="icon fa fa-signal fa-2x"></i> DOLLS</a></li>
+          <li><a href="services.html"><i class="icon fa fa-heart fa-2x"></i> SERVICES</a></li>
+          <li><a href="story.html"><i class="icon fa fa-coffee fa-2x"></i> STORIES</a></li>
 
-<div class="form">
+        </ul>
+  <main class="content">
+<div class="form" align="center"> <br> <br> <br> 
 <h1>Registration</h1>
 <form name="registration" action="" method="post">
-<input type="text" name="username" placeholder="Username" required />
-<input type="email" name="email" placeholder="Email" required />
-<input type="password" name="password" placeholder="Password" required />
+<input type="text" name="username" placeholder="Username" required /> <br>  <br> 
+<input type="email" name="email" placeholder="Email" required /> <br> <br> 
+<input type="password" name="password" placeholder="Password" required /> <br> <br> 
 <input type="submit" name="submit" value="Register" />
 </form>
+</div>
+        </main>
+		
+		    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
 </div>
 </body>
 </html>
